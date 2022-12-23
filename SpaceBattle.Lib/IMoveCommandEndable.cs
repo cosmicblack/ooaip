2b806e2cdf67dbc1e78eaa965428b56a8c68b1b3
cosmicblack <n3xtheshadow@gmail.com>
2022-12-23 23:45:06 +0600

    ustal

---
+++ SpaceBattle.Lib/IMoveCommandEndable.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMoveCommandEndable
+{
+    IUObject UObject { get; }
+    ICommand Command { get; }
+    IQueue<ICommand> Queue { get; }
+    IDictionary<string, object> Properties
+    {
+        get;
+    }
+}
