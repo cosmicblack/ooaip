db3175c9164ad6b1d528e2b17bd1e95207a55653
cosmicblack <n3xtheshadow@gmail.com>
2022-12-23 20:32:28 +0600

    first try

---
+++ SpaceBattle.Lib.Test/EndMoveCommandTests.cs
@@ -0,0 +1,50 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+
+namespace SpaceBattle.Lib.Test;
+
+public class EndMoveCommandTests
+{
+    public EndMoveCommandTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+        var mockCommand = new Mock<ICommand>();
+        mockCommand.Setup(a => a.Execute());
+        var regStrategy = new Mock<IStrategy>();
+        regStrategy.Setup(_s => _s.Execute(It.IsAny<object[]>())).Returns(mockCommand.Object);
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Command.EndMovement", (object[] args) => regStrategy.Object.Execute(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.DeleteProperty", (object[] args) => regStrategy.Object.Execute(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Queue.Push", (object[] args) => regStrategy.Object.Execute(args)).Execute();
+    }
+
+    [Fact]
+    public void PosTestEndMoveCommand()
+    {
+        var m = new Mock<IMoveCommandEndable>();
+        m.SetupGet(a => a.UObject).Returns(new Mock<IUObject>().Object).Verifiable();
+        m.SetupGet(a => a.Properties).Returns(new Dictionary<string, object>() { { "speed", new Vector(It.IsAny<int>(), It.IsAny<int>()) } }).Verifiable();
+        ICommand EndMoveCommand = new EndMoveCommand(m.Object);
+        EndMoveCommand.Execute();
+        m.Verify();
+    }
+    [Fact]
+    public void NegTestEndMoveCommand_UnableToGetUObject()
+    {
+        var m = new Mock<IMoveCommandEndable>();
+        m.SetupGet(a => a.UObject).Throws<Exception>().Verifiable();
+        m.SetupGet(a => a.Properties).Returns(new Dictionary<string, object>() { { "speed", new Vector(It.IsAny<int>(), It.IsAny<int>()) } }).Verifiable();
+        ICommand EndMoveCommand = new EndMoveCommand(m.Object);
+        Assert.Throws<Exception>(() => EndMoveCommand.Execute());
+    }
+    [Fact]
+    public void NegTestEndMoveCommand_UnableToGetSpeed()
+    {
+        var m = new Mock<IMoveCommandEndable>();
+        m.SetupGet(a => a.UObject).Returns(new Mock<IUObject>().Object).Verifiable();
+        m.SetupGet(a => a.Properties).Throws<Exception>().Verifiable();
+        ICommand EndMoveCommand = new EndMoveCommand(m.Object);
+        Assert.Throws<Exception>(() => EndMoveCommand.Execute());
+    }
+}
---
+++ SpaceBattle.Lib/EndMoveCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+public class EndMoveCommand : ICommand
+{
+    public IMoveCommandEndable obj;
+    public EndMoveCommand(IMoveCommandEndable obj)
+    {
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        ICommand EndCommand = IoC.Resolve<ICommand>("Command.EndMovement", obj.UObject);
+        IoC.Resolve<ICommand>("Game.DeleteProperty", obj.UObject, obj.Properties).Execute();
+        IoC.Resolve<ICommand>("Queue.Push", obj.Queue, EndCommand).Execute();
+    }
+}
